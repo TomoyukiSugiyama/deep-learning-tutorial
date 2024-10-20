fcb349cb49016656858be7e35d2811fc4ce22259
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2024-10-20 18:29:58 +0900

    softmax にオーバーフロー対策を追加

--- activation-functions/softmax.go
+++ activation-functions/softmax.go
@@ -3,20 +3,18 @@ package activationfunctions
 import (
 	"math"
 
-	"tutorial/display"
-
 	"gonum.org/v1/gonum/mat"
 )
 
 func Softmax(x *mat.Dense) *mat.Dense {
+	max := mat.Max(x)
 	expf := func(i, j int, x float64) float64 {
-		return math.Exp(x)
+		return math.Exp(x - max)
 	}
 	r, c := x.Dims()
 	exp := mat.NewDense(r, c, nil)
 
 	exp.Apply(expf, x)
-	display.Print(exp)
 	sum := mat.Sum(exp)
 
 	divf := func(i, j int, x float64) float64 {

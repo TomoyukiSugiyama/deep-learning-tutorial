ea9a2642f9b5ed2b8f0c854fc6e5f7ec2ef7e91f
TomoyukiSugiyama <s_tomoyuki07@yahoo.co.jp>
2024-10-21 22:13:12 +0900

    テスト用のコードを分離

--- cmd/main.go
+++ cmd/main.go
@@ -11,28 +11,6 @@ import (
 )
 
 func main() {
-	x := af.Arange(-5.0, 5.0, 0.1)
-	yStep := af.Step(x)
-	display.New(display.Settings{Title: "Step Function", X: "X", Y: "Y", Dataset: display.Dataset{X: x, Y: yStep}, Output: "step.png"}).Show()
-
-	yReLU := af.ReLU(x)
-	display.New(display.Settings{Title: "ReLU Function", X: "X", Y: "Y", Dataset: display.Dataset{X: x, Y: yReLU}, Output: "relu.png"}).Show()
-
-	xVec := mat.NewDense(3, 3, []float64{-4, -3, -2, -1, 0, 1, 2, 3, 4})
-	yVec := af.Sigmoid(xVec)
-	display.Print(yVec)
-
-	xMulVec := mat.NewDense(1, 2, []float64{3, 2})
-	yMulVec := mat.NewDense(1, 2, []float64{2, 4})
-	yMulTVec := yMulVec.T()
-	mul := mat.NewDense(1, 1, nil)
-	mul.Mul(xMulVec, yMulTVec)
-	display.Print(mul)
-
-	xSoftmax := mat.NewDense(1, 3, []float64{0.3, 2.9, 4.0})
-	ySoftmax := af.Softmax(xSoftmax)
-	display.Print(ySoftmax)
-
 	dataset := network.GetData()
 	network := network.InitNetwork()
 
@@ -100,4 +78,28 @@ func maxIndexList(d mat.Matrix) []int {
 		}
 	}
 	return maxIndexList
+}
+
+func Test() {
+	x := af.Arange(-5.0, 5.0, 0.1)
+	yStep := af.Step(x)
+	display.New(display.Settings{Title: "Step Function", X: "X", Y: "Y", Dataset: display.Dataset{X: x, Y: yStep}, Output: "step.png"}).Show()
+
+	yReLU := af.ReLU(x)
+	display.New(display.Settings{Title: "ReLU Function", X: "X", Y: "Y", Dataset: display.Dataset{X: x, Y: yReLU}, Output: "relu.png"}).Show()
+
+	xVec := mat.NewDense(3, 3, []float64{-4, -3, -2, -1, 0, 1, 2, 3, 4})
+	yVec := af.Sigmoid(xVec)
+	display.Print(yVec)
+
+	xMulVec := mat.NewDense(1, 2, []float64{3, 2})
+	yMulVec := mat.NewDense(1, 2, []float64{2, 4})
+	yMulTVec := yMulVec.T()
+	mul := mat.NewDense(1, 1, nil)
+	mul.Mul(xMulVec, yMulTVec)
+	display.Print(mul)
+
+	xSoftmax := mat.NewDense(1, 3, []float64{0.3, 2.9, 4.0})
+	ySoftmax := af.Softmax(xSoftmax)
+	display.Print(ySoftmax)
 }
